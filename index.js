e09a01643cef946b8dbd05c4c2d32c6bb21818c9
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2023-08-11 11:06:01 +0530

    done

---
+++ index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+const { Octokit } = require('@octokit/rest');
+const cron = require('node-cron'); 
+
+const MONGO_URL = process.env.DB;
+const GIT_TOKEN = process.env.TOKEN;
+const GIT_REPO = 'spicybirsge/backups';
+
+async function backup() {
+  console.log("Starting backup");
+  await mongoose.connect(MONGO_URL);
+
+  const backupName = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''); 
+
+  const collections = await mongoose.connection.db.listCollections().toArray();
+
+  for (const collectionInfo of collections) {
+    const collectionName = collectionInfo.name;
+
+    // Use the MongoDB driver to fetch data directly from the collection
+    const collectionData = await mongoose.connection.db.collection(collectionName).find().toArray();
+
+    const dump = {
+      db: mongoose.connection.name,
+      collection: collectionName,
+      data: collectionData
+    };
+
+    const backupJSON = Buffer.from(JSON.stringify(dump)).toString('base64');
+
+    const octokit = new Octokit({ auth: GIT_TOKEN });
+
+    await octokit.repos.createOrUpdateFileContents({
+      owner: GIT_REPO.split('/')[0], 
+      repo: GIT_REPO.split('/')[1],
+      path: `backups/${collectionName}/${backupName}_${collectionName}.json`,
+      message: `Backup ${backupName} - ${collectionName}`,  
+      content: backupJSON
+    });
+
+    console.log(`${backupName}: successfully backed up ${collectionName}`);
+  }
+
+  console.log(`${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}: Backup completed!`);
+
+  mongoose.connection.close();
+}
+
+backup();
